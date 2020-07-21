38813fec4794bacc38965c2b2d608b7a64fa210b
Teresa Heidt <teresaheidt@hotmail.com>
2020-07-21 12:02:55 -0500

    Added functions to the js file

--- quiz.js
+++ quiz.js
@@ -1,6 +1,38 @@
-var button = document.querySelector("#startTimer");
+const startButton = document.getElementById("startBtn")
+const questionContainerElement = document.getElementById("question-container")
 
+const shuffleQuestions, currentQuestionIndex
 
+startButton.addEventListener("click", startGame)
 
+function startGame() {
+    console.log("started")
+    startButton.classList.add("hide")
+    shuffleQuestions = question.sort(() => Math.random( - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
+}
 
-button.addEventListener("click", startTimer);
+function setNextQuestion() {
+    showQuestion(shuffleQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion() {
+    
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        question: 'What Javascript?'
+        answers:[
+            { text: "is programming language that allows you to implement complex features on webpages.", correct: true },
+            { text: " is a styling prgramme to change colors.", correct: false}
+        ]
+    }
+]
